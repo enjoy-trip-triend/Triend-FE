8baf697b78e0da870c7906760ac2aedc60ad24a4
002seojin <002seojin@gmail.com>
2025-06-26 14:10:35 +0900

    feature: websocket util 구현

---
+++ src/utils/websocket.js
@@ -0,0 +1,47 @@
+// utils/websocket.js
+import SockJS from 'sockjs-client';
+import { Client } from '@stomp/stompjs';
+
+let stompClient = null;
+
+export function connectWebSocket(onScheduleUpdate, onEditorUpdate) {
+  stompClient = new Client({
+    webSocketFactory: () => new SockJS('http://localhost:8080/triend-websocket'),
+    debug: (str) => console.log('[WebSocket] ', str),
+    reconnectDelay: 5000,
+    onConnect: () => {
+      console.log('[WebSocket] Connected');
+
+      const plannerId = sessionStorage.getItem('plannerId');
+      const userId = sessionStorage.getItem('userId');
+
+      // 구독
+      stompClient.subscribe(`/topic/planner/${plannerId}/schedule`, (msg) => {
+        onScheduleUpdate(JSON.parse(msg.body));
+      });
+
+      stompClient.subscribe(`/topic/planner/${plannerId}/editors`, (msg) => {
+        onEditorUpdate(JSON.parse(msg.body));
+      });
+
+      // 참여 요청
+      stompClient.publish({
+        destination: '/app/planner/join',
+        body: JSON.stringify({ plannerId: Number(plannerId), userId: Number(userId) }),
+      });
+    },
+  });
+
+  stompClient.activate();
+}
+
+export function sendScheduleUpdate(scheduleMessage) {
+  stompClient.publish({
+    destination: '/app/planner/schedule',
+    body: JSON.stringify(scheduleMessage),
+  });
+}
+
+export function disconnectWebSocket() {
+  if (stompClient) stompClient.deactivate();
+}
