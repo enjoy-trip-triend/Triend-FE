59afe76d18b6087506b463a933aee3cc1a773a81
Kangboom <jkl6147@naver.com>
2025-07-02 14:27:53 +0900

    feature: pinia에 persist 적용

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // src/main.js
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import App from './App.vue'
 import router from './router'
 import Vuetify from './plugins/vuetify'
@@ -8,6 +9,7 @@ import '@/assets/css/global.css'
 
 const app = createApp(App)
 const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate )
 
 app.use(Vuetify)
 app.use(pinia)
